99968edf4e7f768be8626234af1578bdd3ac5bbc
Ayush Kumar <marikayush@gmail.com>
2025-11-09 11:45:33 +0530

    Lecture 48 Math and Date function in JS

---
+++ LEC_48(Inbuild classes in JS)/index.js
@@ -0,0 +1,21 @@
+console.log("Working");
+
+//Math object
+console.log(Math.PI);
+console.log(Math.max(69,59,44,345,67,88,9,56,44,24));
+console.log(Math.min(69,59,44,345,67,88,9,56,44,24));
+console.log(Math.round(66.6));
+console.log(Math.floor(77.8));
+console.log(Math.ceil(77.1));
+console.log(Math.abs(-345));
+console.log(Math.random()); //give random number from 0 to 1
+console.log(Math.pow(2,4));
+
+//Date object
+let date= new Date();
+console.log(date);
+let date2=new Date(2002 , 5, 20, 7);
+console.log(date2);
+console.log(date.getFullYear());
+date.setFullYear(2001);
+console.log(date);
